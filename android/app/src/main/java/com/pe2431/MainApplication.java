6983ed71316cc93f589e1fee0eb7a93c5da15897
george <bakhtarov@gmail.com>
2019-03-10 21:00:10 +0200

    Add/link react-native-video

--- android/app/src/main/java/com/pe2431/MainApplication.java
+++ android/app/src/main/java/com/pe2431/MainApplication.java
@@ -3,6 +3,7 @@ package com.pe2431;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.brentvatne.react.ReactVideoPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactVideoPackage(),
             new RNCameraPackage(),
             new RNGestureHandlerPackage()
       );
