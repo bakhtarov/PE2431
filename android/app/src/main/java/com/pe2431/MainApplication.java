5699ee41d321f5b31e82eae08eb1a302146911ff
george <bakhtarov@gmail.com>
2019-03-12 05:39:31 +0200

    Add react-native-video-editor

--- android/app/src/main/java/com/pe2431/MainApplication.java
+++ android/app/src/main/java/com/pe2431/MainApplication.java
@@ -3,6 +3,7 @@ package com.pe2431;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactlibrary.RNVideoEditorPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNVideoEditorPackage(),
             new ReactVideoPackage(),
             new RNCameraPackage(),
             new RNGestureHandlerPackage()
